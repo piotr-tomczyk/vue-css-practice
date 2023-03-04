606549f1db267b3623ac43c117ba7254722a6fa1
Piotr Tomczyk [Appfire] <piotr.tomczyk@appfire.com>
2023-03-04 15:08:02 +0100

    Change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { fileURLToPath, URL } from 'url';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    base: '/',
+    base: './',
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
